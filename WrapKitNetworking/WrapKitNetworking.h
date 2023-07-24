e7050d71623e656fcfabf3b3b3e91408d9aa22c0
Stas Lee <glees769@gmail.com>
2023-07-24 13:12:34 +0600

    Initial commit

---
+++ WrapKitNetworking/WrapKitNetworking.h
@@ -0,0 +1,18 @@
+//
+//  WrapKitNetworking.h
+//  WrapKitNetworking
+//
+//  Created by Stas Lee on 23/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WrapKitNetworking.
+FOUNDATION_EXPORT double WrapKitNetworkingVersionNumber;
+
+//! Project version string for WrapKitNetworking.
+FOUNDATION_EXPORT const unsigned char WrapKitNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WrapKitNetworking/PublicHeader.h>
+
+
