f619627a92f0cddb410d4550593d9258698e2422
sli <sli@nurtelecom.kg>
2023-11-24 12:00:22 +0600

    Added resource

--- Sources/WrapKitCommon/Presentation/Resource/ResourcePresenter.swift
+++ Sources/WrapKitCommon/Presentation/Resource/ResourcePresenter.swift
@@ -10,6 +10,11 @@ import Foundation
 public struct Resource<Request, Response> {
     public let storage: any Storage<Response>
     public let service: any WrapKit.Service<Request, Response>
+    
+    public init(storage: any Storage<Response>, service: any Service<Request, Response>) {
+        self.storage = storage
+        self.service = service
+    }
 }
 
 public protocol ResourceViewOutput<PresentableModel>: AnyObject {
