fe9254153b1de5286e5ad7c2a7b9a8a1f9c1ff1f
sli <sli@nurtelecom.kg>
2023-12-10 14:57:08 +0600

    Reimplemented Remote service - response handler instead of isresponseok

--- Sources/WrapKitCommon/Service/RemoteService.swift
+++ Sources/WrapKitCommon/Service/RemoteService.swift
@@ -7,27 +7,21 @@
 
 import Foundation
 
-public struct RemoteError: Codable {
-    public let message: String
-    
-    public init(message: String) {
-        self.message = message
-    }
-}
-
 open class RemoteService<Request, Response: Decodable>: Service {
+    public typealias ResponseHandler = ((_ data: Data, _ response: HTTPURLResponse, _ completion: @escaping ((Result<Response, ServiceError>)) -> Void) -> Void)
+     
     private let client: HTTPClient
     private let makeURLRequest: ((Request) -> URLRequest?)
-    private let isResponseOk: ((Data, HTTPURLResponse) -> Bool)?
+    private let responseHandler: ResponseHandler?
     
     public init(
         client: HTTPClient,
         makeURLRequest: @escaping ((Request) -> URLRequest?),
-        isResponseOk: ((Data, HTTPURLResponse) -> Bool)? = nil
+        responseHandler: ResponseHandler?
     ) {
         self.client = client
         self.makeURLRequest = makeURLRequest
-        self.isResponseOk = isResponseOk
+        self.responseHandler = responseHandler
     }
     
     public func make(request: Request, completion: @escaping ((Result<Response, ServiceError>)) -> Void) -> HTTPClientTask? {
@@ -38,47 +32,10 @@ open class RemoteService<Request, Response: Decodable>: Service {
         return client.dispatch(urlRequest) { [weak self] result in
             switch result {
             case let .success(response):
-                self?.handle(response: response, completion: completion)
+                self?.responseHandler?(response.data, response.response, completion)
             case .failure:
                 completion(.failure(.connectivity))
             }
-        }
-    }
-    
-    private func handle(response: (Data, HTTPURLResponse), completion: @escaping ((Result<Response, ServiceError>)) -> Void) {
-        let (data, httpResponse) = response
-
-        if isResponseOk?(data, httpResponse) ?? true {
-            do {
-                let model = try JSONDecoder().decode(Response.self, from: data)
-                completion(.success(model))
-            } catch {
-                printDecodingError(error, data: data)
-                completion(.failure(.internal))
-            }
-        } else if let errorModel = try? JSONDecoder().decode(RemoteError.self, from: data) {
-            completion(.failure(.message(errorModel.message)))
-        } else {
-            completion(.failure(.internal))
-        }
-    }
-
-    private func printDecodingError(_ error: Error, data: Data) {
-        if let decodingError = error as? DecodingError {
-            switch decodingError {
-            case .typeMismatch(let key, let context):
-                print("\nType Mismatch for key: \(key), \(context.debugDescription)\n")
-            case .valueNotFound(let key, let context):
-                print("\nValue not found for key: \(key), \(context.debugDescription)\n")
-            case .keyNotFound(let key, let context):
-                print("\nKey not found: \(key), \(context.debugDescription)\n")
-            case .dataCorrupted(let context):
-                print("\nData corrupted: \(context.debugDescription)\n")
-            @unknown default:
-                print("\nUnknown decoding error: \(decodingError)\n")
-            }
-        } else {
-            print("\nOther error: \(error)\n")
         }
     }
 }
