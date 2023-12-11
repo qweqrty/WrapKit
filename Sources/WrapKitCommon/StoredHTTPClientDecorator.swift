b9e95dc5d7e64c23e504ef7622bd65fb0ac5f122
sli <sli@nurtelecom.kg>
2023-12-11 18:13:19 +0600

    Added storedHTTPclientdecorator

---
+++ Sources/WrapKitCommon/StoredHTTPClientDecorator.swift
@@ -0,0 +1,29 @@
+//
+//  StoredHTTPClientDecorator.swift
+//  WrapKit
+//
+//  Created by Stanislav Li on 11/12/23.
+//
+
+import Foundation
+
+public class StoredHTTPClientDecorator<Model>: HTTPClient {
+    private let decoratee: HTTPClient
+    private let storage: any Storage<Model>
+    private let enrichRequest: ((URLRequest, Model?) -> URLRequest)
+    
+    init(
+        decoratee: HTTPClient,
+        storage: any Storage<Model>,
+        enrichRequest: @escaping ((URLRequest, Model?) -> URLRequest)
+    ) {
+        self.decoratee = decoratee
+        self.storage = storage
+        self.enrichRequest = enrichRequest
+    }
+    
+    public func dispatch(_ request: URLRequest, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        return decoratee.dispatch(enrichRequest(request, storage.get()), completion: completion)
+    }
+}
+
