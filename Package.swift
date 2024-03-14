6681e7176c386d215bfe54d58fd35ba772316a39
Stanislav Li <stanislav@irokez.me>
2024-03-14 13:55:24 +0600

    remove realm dependency

--- Package.swift
+++ Package.swift
@@ -6,36 +6,15 @@ let package = Package(
     name: "WrapKit",
     products: [
         .library(
-            name: "WrapKit",
+            name: "WrapKitStatic",
             type: .static,
             targets: ["WrapKit"]),
-        .library(
-            name: "WrapKitRealm",
-            type: .static,
-            targets: ["WrapKitRealm"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/realm/realm-cocoa", from: "10.14.0")
     ],
     targets: [
         .target(
             name: "WrapKit",
             dependencies: [],
             path: "WrapKitCore/Sources"
-        ),
-        .target(
-            name: "WrapKitRealm",
-            dependencies: [
-                "WrapKit",
-                .product(name: "Realm", package: "realm-cocoa"),
-                .product(name: "RealmSwift", package: "realm-cocoa"),
-            ],
-            path: "WrapKitRealm/Sources"
-        ),
-        .testTarget(
-            name: "WrapKitRealmTests",
-            dependencies: ["WrapKit"],
-            path: "WrapKitRealm/Tests"
         ),
         .testTarget(
             name: "WrapKitTests",
