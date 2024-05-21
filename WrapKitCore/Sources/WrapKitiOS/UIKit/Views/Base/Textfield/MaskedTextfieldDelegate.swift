37c7e2412af7856871517611e49ded0c565f1ec3
Stanislav Li <sli@nurtelecom.kg>
2024-05-21 11:01:42 +0600

    Updated maksed textfield delegate

--- WrapKitCore/Sources/WrapKitiOS/UIKit/Views/Base/Textfield/MaskedTextfieldDelegate.swift
+++ WrapKitCore/Sources/WrapKitiOS/UIKit/Views/Base/Textfield/MaskedTextfieldDelegate.swift
@@ -9,72 +9,77 @@
 import UIKit
 
 public class MaskedTextFieldDelegate: NSObject, UITextFieldDelegate {
-    public let mask: Masking
-    private let textColor: UIColor
-    private let maskTextColor: UIColor
+  public let mask: Masking
+  private let textColor: UIColor
+  private let maskTextColor: UIColor
+  
+  public var userInputText: String { mask.extractUserInput(from: inputText) }
+  public lazy var inputText: String = mask.apply(to: "").input
+  
+  public init(
+    mask: Masking,
+    textColor: UIColor,
+    maskTextColor: UIColor
+  ) {
+    self.mask = mask
+    self.textColor = textColor
+    self.maskTextColor = maskTextColor
+  }
+  
+  public func applyTo(_ textField: Textfield) {
+    textField.delegate = self
+    textField.keyboardType = mask.keyboardType()
+    let mask = mask.apply(to: inputText)
+    let updateText: (() -> Void) = { [weak self] in
+      guard let self = self else { return }
+      self.setupMask(in: textField, mask: self.mask.apply(to: self.inputText))
+    }
+    textField.onBecomeFirstResponder = updateText
+    textField.onResignFirstResponder = updateText
     
-    public var userInputText: String { mask.extractUserInput(from: inputText) }
-    public lazy var inputText: String = mask.apply(to: "").input
+    setupMask(in: textField, mask: mask)
+  }
+  
+  public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    setupMask(
+      in: textField,
+      mask: string.isEmpty ? mask.removeCharacters(from: inputText, in: range) : mask.apply(to: inputText + string)
+    )
+    return false
+  }
+  
+  public func textFieldDidBeginEditing(_ textField: UITextField) {
+    setupMask(in: textField, mask: mask.apply(to: inputText))
+  }
+  
+  private func setupMask(
+    in textField: UITextField,
+    mask: (input: String, maskToInput: String)
+  ) {
+    self.inputText = mask.input
+    textField.text = mask.maskToInput
     
-    public init(
-        mask: Masking,
-        textColor: UIColor,
-        maskTextColor: UIColor
-    ) {
-        self.mask = mask
-        self.textColor = textColor
-        self.maskTextColor = maskTextColor
+    if mask.input.isEmpty && !(textField.placeholder?.isEmpty ?? true) && !textField.isFirstResponder {
+      textField.attributedText = nil
+    } else {
+      textField.attributedText = .combined(
+        .init(mask.input, font: textField.font ?? .systemFont(ofSize: 17), color: textColor, textAlignment: textField.textAlignment),
+        .init(mask.maskToInput, font: textField.font ?? .systemFont(ofSize: 17), color: maskTextColor, textAlignment: textField.textAlignment)
+      )
     }
     
-    public func applyTo(_ textField: UITextField) {
-        textField.delegate = self
-        textField.keyboardType = mask.keyboardType()
-        let mask = mask.apply(to: inputText)
-        
-        self.inputText = mask.input
-        textField.text = mask.maskToInput
-        
-        textField.attributedText = .combined(
-            .init(mask.input, font: textField.font ?? .systemFont(ofSize: 17), color: textColor, textAlignment: textField.textAlignment),
-            .init(mask.maskToInput, font: textField.font ?? .systemFont(ofSize: 17), color: maskTextColor, textAlignment: textField.textAlignment)
-        )
-    }
+    textField.sendActions(for: .editingChanged)
+  }
+  
+  public func textFieldDidChangeSelection(_ textField: UITextField) {
+    guard let selectedRange = textField.selectedTextRange else { return }
     
-    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        setupMask(
-            in: textField,
-            mask: string.isEmpty ? mask.removeCharacters(from: inputText, in: range) : mask.apply(to: inputText + string)
-        )
-        return false
+    let endOfInputIndex = min(inputText.count, textField.text?.count ?? 0)
+    if let endOfInputPosition = textField.position(from: textField.beginningOfDocument, offset: endOfInputIndex) {
+      if textField.offset(from: textField.beginningOfDocument, to: selectedRange.start) > endOfInputIndex {
+        textField.selectedTextRange = textField.textRange(from: endOfInputPosition, to: endOfInputPosition)
+      }
     }
-    
-    public func textFieldDidBeginEditing(_ textField: UITextField) {
-        setupMask(in: textField, mask: mask.apply(to: inputText))
-    }
-    
-    private func setupMask(
-        in textField: UITextField,
-        mask: (input: String, maskToInput: String)
-    ) {
-        self.inputText = mask.input
-        textField.text = mask.maskToInput
-        
-        textField.attributedText = .combined(
-            .init(mask.input, font: textField.font ?? .systemFont(ofSize: 17), color: textColor, textAlignment: textField.textAlignment),
-            .init(mask.maskToInput, font: textField.font ?? .systemFont(ofSize: 17), color: maskTextColor, textAlignment: textField.textAlignment)
-        )
-        
-        textField.sendActions(for: .editingChanged)
-    }
-    
-    public func textFieldDidChangeSelection(_ textField: UITextField) {
-        guard let selectedRange = textField.selectedTextRange else { return }
-        
-        let endOfInputIndex = min(inputText.count, textField.text?.count ?? 0)
-        if let endOfInputPosition = textField.position(from: textField.beginningOfDocument, offset: endOfInputIndex) {
-            if textField.offset(from: textField.beginningOfDocument, to: selectedRange.start) > endOfInputIndex {
-                textField.selectedTextRange = textField.textRange(from: endOfInputPosition, to: endOfInputPosition)
-            }
-        }
-    }
-}#endif
+  }
+}
+#endif
