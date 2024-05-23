a633b7d47b54ceee38652d4afb06398bfb0714a1
Stanislav Li <sli@nurtelecom.kg>
2024-05-23 20:47:20 +0600

    Added default loading output

--- WrapKitCore/Sources/WrapKitCommon/Presentation/UseCases/CommonLoading/CommonLoadingiOSDecorator.swift
+++ WrapKitCore/Sources/WrapKitCommon/Presentation/UseCases/CommonLoading/CommonLoadingiOSDecorator.swift
@@ -8,6 +8,32 @@
 #if canImport(UIKit)
 import UIKit
 
+public extension CommonLoadingOutput where Self == CommonLoadingiOSAdapter {
+static func defaultLoader(
+    onView: UIView,
+    size: CGSize = .init(width: 80, height: 80),
+    loadingViewColor: UIColor,
+    wrapperViewColor: UIColor
+  ) -> CommonLoadingOutput {
+    let loadingView = WrapperView(
+      contentView: NVActivityIndicatorView(
+        frame: .zero,
+        type: .circleStrokeSpin,
+        color: loadingViewColor
+      ),
+      backgroundColor: wrapperViewColor,
+      padding: .init(top: 25, left: 25, bottom: 25, right: 25)
+    )
+    loadingView.cornerRadius = 12
+    loadingView.contentView.startAnimating()
+    return CommonLoadingiOSAdapter(
+      onView: onView,
+      loadingView: loadingView,
+      size: size
+    )
+  }
+}
+
 public class CommonLoadingiOSAdapter: CommonLoadingOutput {
     private let onView: UIView
     private let loadingView: UIView
