c182772001f271ec6b833bbd263f663ab00fb4d6
Stanislav Li <sli@nurtelecom.kg>
2024-05-23 21:04:50 +0600

    Added extension for nv loader

--- WrapKitCore/Sources/WrapKitCommon/Presentation/UseCases/CommonLoading/CommonLoadingiOSDecorator.swift
+++ WrapKitCore/Sources/WrapKitCommon/Presentation/UseCases/CommonLoading/CommonLoadingiOSDecorator.swift
@@ -9,29 +9,30 @@
 import UIKit
 
 public extension CommonLoadingOutput where Self == CommonLoadingiOSAdapter {
-static func defaultLoader(
-    onView: UIView,
-    size: CGSize = .init(width: 80, height: 80),
-    loadingViewColor: UIColor,
-    wrapperViewColor: UIColor
-  ) -> CommonLoadingOutput {
-    let loadingView = WrapperView(
-      contentView: NVActivityIndicatorView(
-        frame: .zero,
-        type: .circleStrokeSpin,
-        color: loadingViewColor
-      ),
-      backgroundColor: wrapperViewColor,
-      padding: .init(top: 25, left: 25, bottom: 25, right: 25)
-    )
-    loadingView.cornerRadius = 12
-    loadingView.contentView.startAnimating()
-    return CommonLoadingiOSAdapter(
-      onView: onView,
-      loadingView: loadingView,
-      size: size
-    )
-  }
+    static func NVActivityLoader(
+        onView: UIView,
+        type: NVActivityIndicatorType = .circleStrokeSpin,
+        size: CGSize = .init(width: 80, height: 80),
+        loadingViewColor: UIColor,
+        wrapperViewColor: UIColor
+    ) -> CommonLoadingOutput {
+        let loadingView = WrapperView(
+            contentView: NVActivityIndicatorView(
+                frame: .zero,
+                type: type,
+                color: loadingViewColor
+            ),
+            backgroundColor: wrapperViewColor,
+            padding: .init(top: 25, left: 25, bottom: 25, right: 25)
+        )
+        loadingView.cornerRadius = 12
+        loadingView.contentView.startAnimating()
+        return CommonLoadingiOSAdapter(
+            onView: onView,
+            loadingView: loadingView,
+            size: size
+        )
+    }
 }
 
 public class CommonLoadingiOSAdapter: CommonLoadingOutput {
