d39c16219fd0822ac76106aff91f5fef8f3f10d2
Stanislav Li <sli@nurtelecom.kg>
2024-06-13 06:48:37 +0600

    Common loading output isloading propertied

--- WrapKitCore/Sources/WrapKitCommon/Presentation/UseCases/CommonLoading/CommonLoadingOutput.swift
+++ WrapKitCore/Sources/WrapKitCommon/Presentation/UseCases/CommonLoading/CommonLoadingOutput.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 public protocol CommonLoadingOutput: AnyObject {
-    func display(isLoading: Bool)
+    var isLoading: Bool { get set }
 }
--- WrapKitCore/Sources/WrapKitCommon/Presentation/UseCases/CommonLoading/CommonLoadingiOSAdapter.swift
+++ WrapKitCore/Sources/WrapKitCommon/Presentation/UseCases/CommonLoading/CommonLoadingiOSAdapter.swift
@@ -36,6 +36,16 @@ public extension CommonLoadingOutput where Self == CommonLoadingiOSAdapter {
 }
 
 public class CommonLoadingiOSAdapter: CommonLoadingOutput {
+    public var isLoading: Bool = false {
+        didSet {
+            isLoading ? onView.showLoadingView(
+                loadingView,
+                backgroundColor: .clear,
+                size: size
+            ) : onView.hideLoadingView()
+        }
+    }
+    
     private let onView: UIView
     private let loadingView: UIView
     private let size: CGSize
@@ -48,14 +58,6 @@ public class CommonLoadingiOSAdapter: CommonLoadingOutput {
         self.onView = onView
         self.loadingView = loadingView
         self.size = size
-    }
-    
-    public func display(isLoading: Bool) {
-        isLoading ? onView.showLoadingView(
-            loadingView,
-            backgroundColor: .clear,
-            size: size
-        ) : onView.hideLoadingView()
     }
 }
 #endif
