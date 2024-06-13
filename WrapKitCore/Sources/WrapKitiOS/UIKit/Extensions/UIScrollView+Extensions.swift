94ad230ce63ca699d5653937f58171a0176d738f
Stanislav Li <sli@nurtelecom.kg>
2024-06-14 01:25:47 +0600

    Added scroll view extension to scroll to any view in its hierarchy

--- WrapKitCore/Sources/WrapKitiOS/UIKit/Extensions/UIScrollView+Extensions.swift
+++ WrapKitCore/Sources/WrapKitiOS/UIKit/Extensions/UIScrollView+Extensions.swift
@@ -12,21 +12,31 @@ public extension UIScrollView {
         guard let targetFrame = view.superview?.convert(view.frame, to: self) else {
             return
         }
-
-        var offset = CGPoint(x: 0, y: targetFrame.origin.y)
         
-        if targetFrame.origin.y + targetFrame.size.height > self.contentSize.height {
-            offset.y = self.contentSize.height - targetFrame.size.height
+        var offset = CGPoint(x: 0, y: targetFrame.origin.y - (self.frame.height / 2) + (targetFrame.height / 2))
+        
+        // Ensure the view is not scrolled out of bounds
+        if targetFrame.origin.y + targetFrame.height > self.contentSize.height {
+            offset.y = self.contentSize.height - self.frame.height
         } else if targetFrame.origin.y < 0 {
             offset.y = 0
         }
-
-        if targetFrame.origin.y + targetFrame.size.height > self.bounds.size.height + self.contentOffset.y {
-            offset.y = targetFrame.origin.y + targetFrame.size.height - self.bounds.size.height
+        
+        // Ensure the view is fully visible within the scroll view
+        if targetFrame.origin.y + targetFrame.height > self.bounds.height + self.contentOffset.y {
+            offset.y = targetFrame.origin.y + targetFrame.height - self.bounds.height
         } else if targetFrame.origin.y < self.contentOffset.y {
             offset.y = targetFrame.origin.y
+        }
+
+        // Ensure the offset does not exceed content size bounds
+        if offset.y > self.contentSize.height - self.bounds.height {
+            offset.y = self.contentSize.height - self.bounds.height
+        } else if offset.y < 0 {
+            offset.y = 0
         }
 
         self.setContentOffset(offset, animated: animated)
     }
 }
+
