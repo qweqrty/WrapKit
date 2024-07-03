719f54412258b0612fd8a64aa52b9f5444040e0a
Stanislav Li <sli@nurtelecom.kg>
2024-07-03 17:14:39 +0600

    Bundle token looking at candidates

--- WrapKitCore/Sources/WrapKitCommon/Resources/BundleToken.swift
+++ WrapKitCore/Sources/WrapKitCommon/Resources/BundleToken.swift
@@ -10,9 +10,21 @@ import Foundation
 public final class BundleToken {
     public static let bundle: Bundle = {
 #if SWIFT_PACKAGE
-        print("dwdw \(Bundle.module.bundlePath)")
         return Bundle.module
 #else
+        // Search for the bundle in the main bundle or among other known bundles
+        let candidates = [
+            Bundle(for: BundleToken.self),
+            Bundle.main,
+        ]
+
+        for candidate in candidates {
+            if let bundlePath = candidate.path(forResource: "WrapKit_WrapKit", ofType: "bundle"),
+               let bundle = Bundle(path: bundlePath) {
+                return bundle
+            }
+        }
+
         return Bundle(for: BundleToken.self)
 #endif
     }()
