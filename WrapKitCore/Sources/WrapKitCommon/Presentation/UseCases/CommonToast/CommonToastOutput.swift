432b9b92f382985c5bbf5ddf83e3a0e388a6ecf4
Stanislav Li <sli@nurtelecom.kg>
2024-07-16 13:17:34 +0600

    Added custom toast

--- WrapKitCore/Sources/WrapKitCommon/Presentation/UseCases/CommonToast/CommonToastOutput.swift
+++ WrapKitCore/Sources/WrapKitCommon/Presentation/UseCases/CommonToast/CommonToastOutput.swift
@@ -12,23 +12,39 @@ public enum CommonToast {
         public let title: String
         public let subtitle: String?
         public let duration: TimeInterval
-        public let trailingView: (String, (() -> Void)?)?
+        public let onPress: (() -> Void)?
         
-        public init(title: String, subtitle: String? = nil, duration: TimeInterval = 3.0, trailingView: (String, (() -> Void)?)? = nil) {
+        public init(
+            title: String,
+            subtitle: String? = nil,
+            duration: TimeInterval = 3.0,
+            onPress: (() -> Void)? = nil
+        ) {
             self.title = title
             self.subtitle = subtitle
             self.duration = duration
-            self.trailingView = trailingView
+            self.onPress = onPress
         }
     }
+    
+    public struct CustomToast {
+        let common: Toast
+        let leadingImage: Image
+        let trailingImage: Image
+        let trailingTitle: String?
+    }
+    
     case error(Toast)
     case success(Toast)
     case warning(Toast)
+    case custom(CustomToast)
     
     public var duration: TimeInterval {
         switch self {
         case .error(let toast), .success(let toast), .warning(let toast):
             return toast.duration
+        case .custom(let toast):
+            return toast.common.duration
         }
     }
 }
