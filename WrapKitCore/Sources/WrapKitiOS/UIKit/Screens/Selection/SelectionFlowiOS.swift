03bf50793b8dfb9cd4cca630293030f7e7bf3ce6
Stanislav Li <sli@nurtelecom.kg>
2024-08-09 10:39:19 +0600

    revert bottom sheet

--- WrapKitCore/Sources/WrapKitiOS/UIKit/Screens/Selection/SelectionFlowiOS.swift
+++ WrapKitCore/Sources/WrapKitiOS/UIKit/Screens/Selection/SelectionFlowiOS.swift
@@ -5,51 +5,51 @@
 //  Created by Daniiar Erkinov on 3/7/24.
 //
 
-//#if canImport(UIKit)
-//import Foundation
-//import UIKit
-//import BottomSheet
-//
-//public class SelectionFlowiOS: SelectionFlow {
-//    public let model: Model
-//    
-//    private weak var navigationController: UINavigationController?
-//    private let factory: any ISelectionFactory<UIViewController>
-//    
-//    public init(
-//        model: Model,
-//        navigationController: UINavigationController?,
-//        factory: any ISelectionFactory<UIViewController>
-//    ) {
-//        self.model = model
-//        self.navigationController = navigationController
-//        self.factory = factory
-//    }
-//    
-//    public func showSelection() {
-//        let vc = factory.resolveSelection(
-//            title: model.title,
-//            isMultipleSelectionEnabled: model.isMultipleSelectionEnabled,
-//            items: model.items,
-//            flow: self,
-//            configuration: model.configuration
-//        )
-//        
-//        navigationController?.presentBottomSheet(
-//            viewController: vc,
-//            configuration: .init(
-//                cornerRadius: 16,
-//                pullBarConfiguration: .hidden,
-//                shadowConfiguration: .init(backgroundColor: model.configuration.content.shadowBackgroundColor)
-//            )
-//        )
-//    }
-//    
-//    public func close(with result: SelectionType?) {
-//        model.callback?(result)
-//        DispatchQueue.main.async {
-//            self.navigationController?.presentedViewController?.dismiss(animated: true)
-//        }
-//    }
-//}
-//#endif
+#if canImport(UIKit)
+import Foundation
+import UIKit
+import BottomSheet
+
+public class SelectionFlowiOS: SelectionFlow {
+    public let model: Model
+    
+    private weak var navigationController: UINavigationController?
+    private let factory: any ISelectionFactory<UIViewController>
+    
+    public init(
+        model: Model,
+        navigationController: UINavigationController?,
+        factory: any ISelectionFactory<UIViewController>
+    ) {
+        self.model = model
+        self.navigationController = navigationController
+        self.factory = factory
+    }
+    
+    public func showSelection() {
+        let vc = factory.resolveSelection(
+            title: model.title,
+            isMultipleSelectionEnabled: model.isMultipleSelectionEnabled,
+            items: model.items,
+            flow: self,
+            configuration: model.configuration
+        )
+        
+        navigationController?.presentBottomSheet(
+            viewController: vc,
+            configuration: .init(
+                cornerRadius: 16,
+                pullBarConfiguration: .hidden,
+                shadowConfiguration: .init(backgroundColor: model.configuration.content.shadowBackgroundColor)
+            )
+        )
+    }
+    
+    public func close(with result: SelectionType?) {
+        model.callback?(result)
+        DispatchQueue.main.async {
+            self.navigationController?.presentedViewController?.dismiss(animated: true)
+        }
+    }
+}
+#endif
