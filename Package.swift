718e550a9ef4e5222ef02e205fe7cfe24f0404f1
Stanislav Li <sli@nurtelecom.kg>
2024-09-04 11:47:28 +0600

    fix build package

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
             targets: ["WrapKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/joomcode/BottomSheet", from: "2.0.5"),
-        .package(url: "https://github.com/shu223/Pulsator", from: "0.6.3"),
+        .package(url: "https://github.com/joomcode/BottomSheet.git", from: "2.0.5"),
+        .package(url: "https://github.com/shu223/Pulsator.git", from: "0.6.3"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.12.0")
     ],
     targets: [
@@ -30,8 +30,8 @@ let package = Package(
             name: "WrapKit",
             dependencies: [
                 .product(name: "BottomSheet", package: "BottomSheet", condition: .when(platforms: [.iOS])),
-                .product(name: "Kingfisher", package: "Kingfisher", condition: .when(platforms: [.iOS])),
-                "Pulsator"
+                .product(name: "Pulsator", package: "Pulsator", condition: .when(platforms: [.iOS])),
+                .product(name: "Kingfisher", package: "Kingfisher", condition: .when(platforms: [.iOS]))
             ],
             path: "Sources/WrapKit"
         ),
