6c61c07e06d194aca7bed9aefee9af1ecae121f6
Stanislav Li <stanislav@irokez.me>
2024-09-18 15:14:19 +0600

    added updateAppearance for TextView

--- WrapKitCore/Sources/WrapKitiOS/UIKit/Views/Base/Textview.swift
+++ WrapKitCore/Sources/WrapKitiOS/UIKit/Views/Base/Textview.swift
@@ -10,9 +10,12 @@ import UIKit
 
 open class Textview: UITextView, UITextViewDelegate {
     private var padding: UIEdgeInsets
+    private var isValidState = true
+    
     public lazy var placeholderLabel = Label(font: font!, textColor: .gray)
     public var textDidChange: (() -> Void)?
     public var shouldChangeText: ((NSRange, String) -> Bool)?
+    public var appearance: TextfieldAppearance { didSet { updateAppearance() }}
     
     open override var semanticContentAttribute: UISemanticContentAttribute {
         didSet {
@@ -23,30 +26,26 @@ open class Textview: UITextView, UITextViewDelegate {
     }
     
     public init(
-        font: UIFont = .systemFont(ofSize: 16),
-        textColor: UIColor = .black,
-        backgroundColor: UIColor = .clear,
+        appearance: TextfieldAppearance,
         cornerRadius: CGFloat = 10,
-        placeholder: String = "",
-        placeholderTextColor: UIColor = .gray,
         borderWidth: CGFloat = 1,
-        borderColor: UIColor = .darkGray,
         contentInset: UIEdgeInsets = .init(top: 12, left: 12, bottom: 12, right: 12)
     ) {
         self.padding = contentInset
+        self.appearance = appearance
         super.init(frame: .zero, textContainer: nil)
-        self.font = font
-        self.textColor = textColor
+        self.font = appearance.font
+        self.textColor = appearance.colors.textColor
         self.textContainerInset = padding
-        self.backgroundColor = backgroundColor
+        self.backgroundColor = appearance.colors.deselectedBackgroundColor
         self.contentMode = contentMode
-        self.layer.borderColor = borderColor.cgColor
+        self.layer.borderColor = appearance.colors.deselectedBorderColor.cgColor
         self.layer.borderWidth = borderWidth
         self.layer.cornerRadius = cornerRadius
         self.textContainerInset = contentInset
         self.delegate = self
-        self.placeholderLabel.textColor = placeholderTextColor
-        self.placeholderLabel.font = font
+        self.placeholderLabel.textColor = appearance.placeholder?.color
+        self.placeholderLabel.font = appearance.placeholder?.font
         setupSubviews()
         setupConstraints()
     }
@@ -62,9 +61,9 @@ open class Textview: UITextView, UITextViewDelegate {
         )
     }
     
-    public required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         self.padding = .zero
-        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
     }
     
     public func textViewDidChange(_ textView: UITextView) {
@@ -74,6 +73,55 @@ open class Textview: UITextView, UITextViewDelegate {
     
     public func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         return shouldChangeText?(range, text) ?? true
+    }
+    
+    private func updatePlaceholder() {
+        guard let customizedPlaceholder = appearance.placeholder else { return }
+        attributedText = NSAttributedString(
+            string: customizedPlaceholder.text ?? placeholderLabel.text ?? "",
+            attributes: [
+                NSAttributedString.Key.foregroundColor: isUserInteractionEnabled ? customizedPlaceholder.color : (customizedPlaceholder.disabledColor ?? customizedPlaceholder.color),
+                NSAttributedString.Key.font: customizedPlaceholder.font
+            ]
+        )
+    }
+}
+
+public extension Textview {
+    func updateAppearance(isValid: Bool) {
+        self.isValidState = isValid
+        font = appearance.font
+        let isFirstResponder = isFirstResponder
+        let appearance = appearance
+        UIView.animate(withDuration: 0.1, delay: .leastNonzeroMagnitude, options: [.allowUserInteraction]) {
+            if isValid {
+                self.backgroundColor = isFirstResponder ? appearance.colors.selectedBackgroundColor : appearance.colors.deselectedBackgroundColor
+                self.layer.borderColor = isFirstResponder ? appearance.colors.selectedBorderColor.cgColor : appearance.colors.deselectedBorderColor.cgColor
+            } else {
+                self.backgroundColor = appearance.colors.errorBackgroundColor
+                self.layer.borderColor = appearance.colors.errorBorderColor.cgColor
+            }
+            self.layer.borderWidth = (isFirstResponder ? appearance.border?.selectedBorderWidth : appearance.border?.idleBorderWidth) ?? 0
+        }
+    }
+    
+    private func updateAppearance() {
+        updatePlaceholder()
+        font = appearance.font
+        let text = (delegate as? MaskedTextfieldDelegate)?.fullText ?? text
+        let isValid = isValidState
+        let isFirstResponder = isFirstResponder
+        let appearance = appearance
+        UIView.animate(withDuration: 0.1, delay: .leastNonzeroMagnitude, options: [.allowUserInteraction]) {
+            if isValid {
+                self.backgroundColor = isFirstResponder ? appearance.colors.selectedBackgroundColor : appearance.colors.deselectedBackgroundColor
+                self.layer.borderColor = isFirstResponder ? appearance.colors.selectedBorderColor.cgColor : appearance.colors.deselectedBorderColor.cgColor
+            } else {
+                self.backgroundColor = appearance.colors.errorBackgroundColor
+                self.layer.borderColor = appearance.colors.errorBorderColor.cgColor
+            }
+            self.layer.borderWidth = (isFirstResponder ? appearance.border?.selectedBorderWidth : appearance.border?.idleBorderWidth) ?? 0
+        }
     }
 }
 #endif
