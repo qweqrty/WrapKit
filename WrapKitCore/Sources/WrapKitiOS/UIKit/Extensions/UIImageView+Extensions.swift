43d1725289db69961e2ef2fea841a30b2119e967
Stanislav Li <sli@nurtelecom.kg>
2024-10-02 00:39:47 +0600

    Set image

--- WrapKitCore/Sources/WrapKitiOS/UIKit/Extensions/UIImageView+Extensions.swift
+++ WrapKitCore/Sources/WrapKitiOS/UIKit/Extensions/UIImageView+Extensions.swift
@@ -9,14 +9,31 @@
 import UIKit
 
 public extension UIImageView {
-    func setImageWithAnimation(image: UIImage?, with animation: UIView.AnimationOptions = .transitionCrossDissolve) {
-        UIView.transition(with: self,
-                          duration: 0.3,
-                          options: .transitionCrossDissolve,
-                          animations: { [weak image] in
-            self.image = image
-        },
-                          completion: nil)
+    func setImage(
+        image: ImageEnum?,
+        animation: UIView.AnimationOptions = .transitionCrossDissolve,
+        viewWhileLoading: UIView?
+    ) {
+        if let viewWhileLoading {
+            addSubview(viewWhileLoading)
+            viewWhileLoading.fillSuperview()
+        }
+        switch image {
+        case .asset(let image):
+            UIView.transition(with: self,
+                              duration: 0.3,
+                              options: .transitionCrossDissolve,
+                              animations: { [weak self] in
+                self?.image = image
+            },
+                              completion: nil)
+        case .url(let string):
+            kf.setImage(with: URL(string: string)) { [weak viewWhileLoading] result in
+                viewWhileLoading?.removeFromSuperview()
+            }
+        default:
+            break
+        }
     }
 }
 #endif
