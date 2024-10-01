db106cf3ecd2c11fb61e77ae270ffff9697039a6
Stanislav Li <sli@nurtelecom.kg>
2024-10-02 00:39:47 +0600

    Set image

--- WrapKitCore/Sources/WrapKitiOS/UIKit/Extensions/UIImageView+Extensions.swift
+++ WrapKitCore/Sources/WrapKitiOS/UIKit/Extensions/UIImageView+Extensions.swift
@@ -7,16 +7,63 @@
 
 #if canImport(UIKit)
 import UIKit
+import Kingfisher
 
 public extension UIImageView {
-    func setImageWithAnimation(image: UIImage?, with animation: UIView.AnimationOptions = .transitionCrossDissolve) {
-        UIView.transition(with: self,
-                          duration: 0.3,
-                          options: .transitionCrossDissolve,
-                          animations: { [weak image] in
-            self.image = image
-        },
-                          completion: nil)
+    func setImage(
+        image: ImageEnum?,
+        animation: UIView.AnimationOptions = .transitionCrossDissolve,
+        viewWhileLoading: UIView? = nil,
+        fallbackView: View? = nil
+    ) {
+        switch image {
+        case .asset(let image):
+            animatedSet(image)
+        case .url(let string):
+            guard let url = URL(string: string) else { return }
+            loadImage(url, viewWhileLoading: viewWhileLoading, fallbackView: fallbackView)
+        default:
+            break
+        }
+    }
+    
+    private func loadImage(_ url: URL, viewWhileLoading: UIView?, fallbackView: View?) {
+        fallbackView?.removeFromSuperview()
+        if let viewWhileLoading {
+            addSubview(viewWhileLoading)
+            viewWhileLoading.fillSuperview()
+        }
+        KingfisherManager.shared.retrieveImage(with: url) { [weak self, weak viewWhileLoading, url] result in
+            viewWhileLoading?.removeFromSuperview()
+            switch result {
+            case .success(let image):
+                self?.animatedSet(image.image)
+            case .failure:
+                self?.addFallbackView(url, viewWhileLoading: viewWhileLoading, fallbackView: fallbackView)
+            }
+        }
+    }
+    
+    private func addFallbackView(_ url: URL, viewWhileLoading: UIView?, fallbackView: View?) {
+        guard let fallbackView = fallbackView else { return }
+        fallbackView.removeFromSuperview()
+        addSubview(fallbackView)
+        fallbackView.centerInSuperview()
+        fallbackView.onPress = { [weak self] in
+            self?.loadImage(url, viewWhileLoading: viewWhileLoading, fallbackView: fallbackView)
+        }
+    }
+    
+    private func animatedSet(_ image: UIImage?) {
+        UIView.transition(
+            with: self,
+            duration: 0.3,
+            options: .transitionCrossDissolve,
+            animations: { [weak self] in
+                self?.image = image
+            },
+            completion: nil
+        )
     }
 }
 #endif
