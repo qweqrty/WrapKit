ef8a780e3979809123fc98567bdc34f5c6f6ab35
Stanislav Li <sli@nurtelecom.kg>
2024-10-04 02:32:57 +0600

    Image view fixed

--- WrapKitCore/Sources/WrapKitiOS/UIKit/Views/Base/ImageView.swift
+++ WrapKitCore/Sources/WrapKitiOS/UIKit/Views/Base/ImageView.swift
@@ -11,14 +11,33 @@ import UIKit
 open class ImageView: UIImageView {
     public var onPress: (() -> Void)? {
         didSet {
-            isUserInteractionEnabled = onPress != nil
-            removeGestureRecognizer(tapGesture)
+            removeGestureRecognizer(tapGestureRecognizer)
             guard onPress != nil else { return }
-            addGestureRecognizer(tapGesture)
+            addGestureRecognizer(tapGestureRecognizer)
         }
     }
     
-    private lazy var tapGesture = UITapGestureRecognizer(target: self, action: #selector(onTap))
+    public var onLongPress: (() -> Void)? {
+        didSet {
+            removeGestureRecognizer(longPressRecognizer)
+            guard onLongPress != nil else { return }
+            addGestureRecognizer(longPressRecognizer)
+        }
+    }
+    
+    lazy var tapGestureRecognizer: UITapGestureRecognizer = {
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(didTap))
+        recognizer.numberOfTapsRequired = 1
+        recognizer.cancelsTouchesInView = false
+        return recognizer
+    }()
+    
+    lazy var longPressRecognizer: UILongPressGestureRecognizer = {
+        let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(didLongPress))
+        recognizer.minimumPressDuration = 1
+        recognizer.cancelsTouchesInView = false
+        return recognizer
+    }()
     
     public override var tintColor: UIColor! {
         didSet {
@@ -35,7 +54,7 @@ open class ImageView: UIImageView {
         super.traitCollectionDidChange(previousTraitCollection)
         
         if #available(iOS 13.0, *), let image = self.image, image.renderingMode == .alwaysTemplate {
-            setImage(image: .asset(self.image?.withTintColor(tintColor))) 
+            setImage(image: .asset(self.image?.withTintColor(tintColor)))
         }
     }
     
@@ -46,8 +65,7 @@ open class ImageView: UIImageView {
         borderColor: UIColor = .clear,
         borderWidth: CGFloat = 0,
         tintColor: UIColor? = nil,
-        isHidden: Bool = false,
-        isUserInteractionEnabled: Bool = false
+        isHidden: Bool = false
     ) {
         super.init(image: nil)
         setImage(image: .asset(image))
@@ -57,31 +75,59 @@ open class ImageView: UIImageView {
             self.tintColor = tintColor
         }
         self.cornerRadius = cornerRadius
-        self.isUserInteractionEnabled = isUserInteractionEnabled
     }
-  
-  public required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  @objc private func onTap() {
-      self.alpha = 1.0
-      UIView.animate(withDuration: 0.4, delay: 0.0, options: .allowUserInteraction, animations: {
+    
+    public override init(image: UIImage?) {
+        super.init(image: nil)
+        setImage(image: .asset(image))
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setImage(image: .asset(nil))
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func didTap() {
+        onPress?()
+    }
+    
+    @objc private func didLongPress() {
+        onLongPress?()
+    }
+    
+    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        layoutIfNeeded()
+        super.touchesBegan(touches, with: event)
+        guard onLongPress != nil || onPress != nil else { return }
         self.alpha = 0.5
-      }, completion: { finished in
-        self.alpha = 1.0
-      })
-    onPress?()
-  }
-    
-    override public func layoutSubviews() {
-        super.layoutSubviews()
-        clipsToBounds = true
     }
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.contentMode = .scaleAspectFit
+    override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 6, options: .allowUserInteraction) { [weak self] in
+            self?.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }
+        super.touchesEnded(touches, with: event)
+        guard onLongPress != nil || onPress != nil else { return }
+        
+        UIView.animate(withDuration: 0.3, delay: 0, options: [.allowUserInteraction]) {
+            self.alpha = 1.0
+        }
+    }
+    
+    override open func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 6, options: .allowUserInteraction) { [weak self] in
+            self?.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }
+        super.touchesCancelled(touches, with: event)
+        guard onLongPress != nil || onPress != nil else { return }
+        
+        UIView.animate(withDuration: 0.3, delay: 0, options: [.allowUserInteraction]) {
+            self.alpha = 1.0
+        }
     }
 }
 #endif
--- WrapKitCore/Sources/WrapKitiOS/UIKit/Views/Custom/SearchBar.swift
+++ WrapKitCore/Sources/WrapKitiOS/UIKit/Views/Custom/SearchBar.swift
@@ -39,7 +39,11 @@ public class SearchBar: View {
         stackView.addArrangedSubview(textfield)
         stackView.addArrangedSubview(rightView)
         
+        leftView.setContentHuggingPriority(.required, for: .horizontal)
+        rightView.setContentHuggingPriority(.required, for: .horizontal)
+        
         leftView.setContentCompressionResistancePriority(.required, for: .horizontal)
+        textfield.setContentCompressionResistancePriority(.required, for: .horizontal)
         rightView.setContentCompressionResistancePriority(.required, for: .horizontal)
     }
     
