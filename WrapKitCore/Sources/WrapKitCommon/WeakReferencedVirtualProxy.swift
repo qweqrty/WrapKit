dcafed1f6b0b642411903904be41495da3091b0f
Stanislav Li <sli@nurtelecom.kg>
2024-10-17 14:49:01 +0600

    sort folders

---
+++ WrapKitCore/Sources/WrapKitCommon/WeakReferencedVirtualProxy.swift
@@ -0,0 +1,30 @@
+//
+//  WeakRefVirtualProxy.swift
+//
+//
+//  Created by Stanislav Li on 17/10/24.
+//
+
+import Foundation
+
+public final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    public init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension CommonLoadingOutput {
+    public var weakReferenced: CommonLoadingOutput {
+        return WeakRefVirtualProxy(self)
+    }
+}
+
+extension WeakRefVirtualProxy: CommonLoadingOutput where T: CommonLoadingOutput {
+    public var isLoading: Bool { object?.isLoading == true }
+    
+    public func display(isLoading: Bool) {
+        object?.display(isLoading: isLoading)
+    }
+}
