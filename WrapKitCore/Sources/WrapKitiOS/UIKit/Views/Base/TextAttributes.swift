253e993dc2a5a668527ec6c579275c6ac555b6a3
gzheenbekkyzy <gzheenbekkyzy@nurtelecom.kg>
2024-10-22 17:49:14 +0600

    DOM-2537 copy pasted Lee's code

--- WrapKitCore/Sources/WrapKitiOS/UIKit/Views/Base/TextAttributes.swift
+++ WrapKitCore/Sources/WrapKitiOS/UIKit/Views/Base/TextAttributes.swift
@@ -15,53 +15,6 @@ import AppKit
 public typealias UnderlineStyle = NSUnderlineStyle
 public typealias TextAlignment = NSTextAlignment
 
-#elseif canImport(WatchKit)
-import WatchKit
-public typealias UnderlineStyle = NSUnderlineStyle
-public typealias TextAlignment = NSTextAlignment
-
-#elseif canImport(SwiftUI)
-import SwiftUI
-public typealias UnderlineStyle = UnderlineStyleWrapper // Custom type for SwiftUI
-public typealias TextAlignment = TextAlignmentWrapper // Custom type for SwiftUI
-
-public struct UnderlineStyleWrapper {
-    public var style: NSUnderlineStyle?
-
-    public init(style: NSUnderlineStyle? = nil) {
-        self.style = style
-    }
-    
-    // Apply underline in SwiftUI
-    public func apply(to text: Text) -> Text {
-        if let style = style, style.contains(.single) {
-            return text.underline(true)
-        }
-        return text
-    }
-}
-
-public struct TextAlignmentWrapper {
-    public var alignment: TextAlignment?
-
-    public init(alignment: TextAlignment? = nil) {
-        self.alignment = alignment
-    }
-    
-    // Convert TextAlignment to SwiftUI equivalent
-    public func alignmentForSwiftUI() -> Alignment {
-        switch alignment {
-        case .leading:
-            return .leading
-        case .center:
-            return .center
-        case .trailing:
-            return .trailing
-        default:
-            return .leading
-        }
-    }
-}
 #endif
 
 public struct TextAttributes {
