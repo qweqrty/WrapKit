cfe63de7af5219fb57e03771636e28fa81a851ec
Stanislav Li <sli@nurtelecom.kg>
2024-10-30 14:54:26 +0600

    Add imei on device

---
+++ WrapKitCore/Sources/WrapKitCommon/Helpers/DeviceKit+Extensions.swift
@@ -0,0 +1,14 @@
+import DeviceKit
+#if canImport(UIKit)
+import UIKit
+#endif
+
+public extension Device {
+    var imei: String {
+            #if canImport(UIKit)
+            return UIDevice.current.identifierForVendor?.uuidString ?? UUID().uuidString
+            #else
+            return UUID().uuidString
+            #endif
+    }
+}
