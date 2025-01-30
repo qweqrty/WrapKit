f1787cc9b01fd562c276fa22021b18fd32094a00
Daniiar Erkinov <dan4111k1@gmail.com>
2025-01-30 11:58:36 +0600

    Move PreferredSheetSizing

--- WrapKitCore/Sources/WrapKitiOS/UIKit/Screens/BottomSheet/BottomSheetController.swift
+++ WrapKitCore/Sources/WrapKitiOS/UIKit/Screens/BottomSheet/BottomSheetController.swift
@@ -2,50 +2,6 @@
 import UIKit
 
 open class BottomSheetController<View: UIView>: ViewController<View> {
-    public enum PreferredSheetSizing {
-        case fit // Fit, based on the view's constraints
-        case small
-        case medium
-        case large
-        case fill
-        case custom(value: CGFloat)
-        
-        /// В .custom нужно указать процент высоты боттом щита относительно высоты экрана. Можете записать в дробном виде 0.3, или в виде числа
-        /// 30 (это будет эквивалентно 30%).
-        public var size: CGFloat {
-            switch self {
-            case .fit:
-                return 0
-            case .small:
-                return 0.25
-            case .medium:
-                return 0.5
-            case .large:
-                return 0.85
-            case .fill:
-                return 1
-            case .custom(let value):
-                return validateCustomSize(value)
-            }
-        }
-        
-        private func validateCustomSize(_ value: CGFloat) -> CGFloat {
-            guard value >= 0 else {
-                return 0
-            }
-            
-            guard value <= 100 else {
-                return 1
-            }
-            
-            guard value <= 1 else {
-                return value / 100
-            }
-            
-            return value
-        }
-    }
-
     private lazy var bottomSheetTransitioningDelegate = BottomSheetTransitioningDelegate(
         preferredSheetTopInset: preferredSheetTopInset,
         preferredSheetLeftInset: preferredSheetLeftInset,
@@ -142,7 +98,7 @@ open class BottomSheetController<View: UIView>: ViewController<View> {
         }
     }
     
-    open var preferredSheetSizing: PreferredSheetSizing = .fit {
+    open var preferredSheetSizing: BottomSheetPreferredSheetSizing = .fit {
         didSet {
             bottomSheetTransitioningDelegate.preferredSheetSizingFactor = preferredSheetSizing.size
         }
