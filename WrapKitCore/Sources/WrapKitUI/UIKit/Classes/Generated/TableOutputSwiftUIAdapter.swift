63170b814fcc956d85e1dace4caa63a38b9afd9b
Stanislav Li <sli@nurtelecom.kg>
2025-03-31 16:06:09 +0600

    generate outputs

---
+++ WrapKitCore/Sources/WrapKitUI/UIKit/Classes/Generated/TableOutputSwiftUIAdapter.swift
@@ -0,0 +1,36 @@
+// Generated using Sourcery 2.2.6 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+// swiftlint:disable:this file_name
+// swiftlint:disable all
+// swift-format-ignore-file
+// swiftformat:disable all
+
+#if canImport(WrapKit)
+import WrapKit
+#endif
+#if canImport(SwiftUI)
+import SwiftUI
+#endif
+#if canImport(Foundation)
+import Foundation
+#endif
+#if canImport(UIKit)
+import UIKit
+#endif
+public class TableOutputSwiftUIAdapter<CellFooterHeader>: ObservableObject, TableOutput {
+
+    // Initializer
+    public init(
+    ) {
+    }
+
+    @Published public var displaySectionsState: DisplaySectionsState? = nil
+    public struct DisplaySectionsState {
+        public let sections: [TableSection<Header, Cell, Footer>]
+    }
+    public func display(sections: [TableSection<Header, Cell, Footer>]) {
+        displaySectionsState = .init(
+            sections: sections
+        )
+    }
+}
