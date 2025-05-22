0efb6e700549f585aff2a7fb56be822a1b39cab6
Stanislav Li <sli@nurtelecom.kg>
2025-05-22 11:16:21 +0600

    exact kingfisher

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.3"),
         .package(url: "https://github.com/airbnb/lottie-spm", from: "4.5.0"),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.12.0"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", exact: "7.12.0"),
         .package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "4.0.0"),
         .package(url: "https://github.com/devicekit/DeviceKit", from: "5.5.0")
     ],
