22088fb711f61c4279542bfb68c919af21197962
Stanislav Li <sli@nurtelecom.kg>
2025-05-30 16:16:06 +0600

    Add MockHTTPClient

--- WrapKitCore/Sources/WrapKitNetworking/HTTPClient.swift
+++ WrapKitCore/Sources/WrapKitNetworking/HTTPClient.swift
@@ -52,3 +52,45 @@ public protocol HTTPDownloadClient {
     @discardableResult
     func download(_ request: URLRequest, progress: @escaping (Double) -> Void, completion: @escaping (DownloadResult) -> Void) -> HTTPClientTask
 }
+
+public class MockHTTPClient: HTTPClient {
+    private class MockHTTPClientTask: HTTPClientTask {
+        private let resumeAction: () -> Void
+        private var isCancelled = false
+        init(resumeAction: @escaping () -> Void) {
+            self.resumeAction = resumeAction
+        }
+        func resume() {
+            guard !isCancelled else { return }
+            resumeAction()
+        }
+        func cancel() {
+            isCancelled = true
+        }
+    }
+    
+    private let responses: [Data]
+    private var responseIndex: Int = 0
+    
+    public init(jsonStrings: [String]) {
+        self.responses = jsonStrings.compactMap { $0.data(using: .utf8) }
+    }
+    
+    public func dispatch(_ request: URLRequest, completion: @escaping (Result<(data: Data, response: HTTPURLResponse), Error>) -> Void) -> HTTPClientTask {
+        let mockTask = MockHTTPClientTask { [weak self] in
+            guard let self else { return }
+            if responseIndex < responses.count, let url = request.url, let data = responses.item(at: responseIndex), let httpResponse = HTTPURLResponse(
+                url: url,
+                statusCode: 200,
+                httpVersion: "1.1",
+                headerFields: ["Content-Type": "application/json"]
+            ) {
+                completion(.success((data: data, response: httpResponse)))
+                responseIndex += 1
+            } else {
+                completion(.failure(NSError(domain: NSURLErrorDomain, code: NSURLErrorDataNotAllowed, userInfo: nil)))
+            }
+        }
+        return mockTask
+    }
+}
