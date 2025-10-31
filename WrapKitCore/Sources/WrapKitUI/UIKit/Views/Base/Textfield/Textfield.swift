0a3f292df72f01cf57f7c60271b5bc5e81f6061c
gzheenbekkyzy <gzheenbekkyzy@nurtelecom.kg>
2025-10-31 14:41:28 +0600

    DOM-3492

--- WrapKitCore/Sources/WrapKitUI/UIKit/Views/Base/Textfield/Textfield.swift
+++ WrapKitCore/Sources/WrapKitUI/UIKit/Views/Base/Textfield/Textfield.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
-#if canImport(UIKit)
-import UIKit
-public typealias TextAutocapitalizationType = UITextAutocapitalizationType
-#endif
-// TODO: UITextAutocapitalizationType in SwiftUI and AppKit
+public enum TextAutocapitalizationType {
+    case none
+    case words
+    case sentences
+    case allCharacters
+}
 
 public struct TextfieldAppearance {
     public init(
@@ -234,7 +235,7 @@ public struct TextInputPresentableModel: HashableWithReflection {
 #if canImport(UIKit)
 import UIKit
 
-public extension TextInputOutput {
+public extension Textfield {
     func makeAccessoryView(
         accessoryView: UIView,
         height: CGFloat = 60,
@@ -300,8 +301,8 @@ extension Textfield: TextInputOutput {
             self.inputAccessoryView = nil
             self.reloadInputViews()
         }
-        if let autocapitalizationType = model.autocapitalizationType {
-            self.autocapitalizationType = autocapitalizationType
+        if let type = model.autocapitalizationType {
+            self.autocapitalizationType = type.asUITextAutocapitalizationType
         }
         
         if let inputType = model.inputType {
@@ -566,7 +567,7 @@ open class Textfield: UITextField {
         self.cornerRadius = cornerRadius
         self.autocorrectionType = .no
         self.textColor = appearance.colors.textColor
-        self.autocapitalizationType = .none
+        self.autocapitalizationType = autocapitalizationType
         self.inputView = inputView
         maskedTextfieldDelegate = delegate
         delegate?.applyTo(textfield: self)
@@ -832,6 +833,17 @@ public extension Textfield {
                 self.layer.borderColor = isFirstResponder ? appearance.colors.selectedErrorBorderColor.cgColor : appearance.colors.errorBorderColor.cgColor
             }
             self.layer.borderWidth = (isFirstResponder ? appearance.border?.selectedBorderWidth : appearance.border?.idleBorderWidth) ?? 0
+        }
+    }
+}
+
+private extension TextAutocapitalizationType {
+    var asUITextAutocapitalizationType: UITextAutocapitalizationType {
+        switch self {
+        case .allCharacters: return .allCharacters
+        case .sentences: return .sentences
+        case .words: return .words
+        case .none: return .none
         }
     }
 }
