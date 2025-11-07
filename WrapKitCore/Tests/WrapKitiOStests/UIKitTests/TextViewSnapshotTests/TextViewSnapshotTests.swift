0502b6c66215d6b925d1f7bd49a924bbcd45cec1
isagulovurmat <falone641234@gmail.com>
2025-11-07 10:41:02 +0600

    Created tests file

---
+++ WrapKitCore/Tests/WrapKitiOStests/UIKitTests/TextViewSnapshotTests/TextViewSnapshotTests.swift
@@ -0,0 +1,14 @@
+//
+//  TextViewSnapshotTests.swift
+//  WrapKitTests
+//
+//  Created by sunflow on 7/11/25.
+//
+
+import WrapKit
+import XCTest
+import WrapKitTestUtils
+
+final class TextViewSnapshotTests: XCTestCase {
+    
+}
