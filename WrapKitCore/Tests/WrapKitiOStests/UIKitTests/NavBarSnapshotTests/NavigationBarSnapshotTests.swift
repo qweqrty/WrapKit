bce181abf014cb9f11676f9103820ce4225badd9
isagulovurmat <falone641234@gmail.com>
2025-11-11 10:23:41 +0600

    Navbar additional snapshot tests

--- WrapKitCore/Tests/WrapKitiOStests/UIKitTests/NavBarSnapshotTests/NavigationBarSnapshotTests.swift
+++ WrapKitCore/Tests/WrapKitiOStests/UIKitTests/NavBarSnapshotTests/NavigationBarSnapshotTests.swift
@@ -92,10 +92,10 @@ class NavigationBarSnapshotTests: XCTestCase {
             secondaryColor: .green)
         )
         
-        sut.display(leadingCard: .init(backgroundImage: .init(size: CGSize(width: 25, height: 25), image: .asset(Image(systemName: "star.fill"))), title: .text("Title")))
+        sut.display(leadingCard: .init(backgroundImage: .init(image: .asset(Image(systemName: "star.fill"))), title: .text("Title")))
         
         // THEN
-        assert(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "NAVBAR_WITH_LEADINGCARD_BACKGROUNDIMAGE_TITLE_LIGHT")
+        record(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "NAVBAR_WITH_LEADINGCARD_BACKGROUNDIMAGE_TITLE_LIGHT")
         assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "NAVBAR_WITH_LEADINGCARD_BACKGROUNDIMAGE_TITLE_DARK")
     }
     
@@ -113,11 +113,170 @@ class NavigationBarSnapshotTests: XCTestCase {
             secondaryColor: .green)
         )
         
-        sut.display(leadingCard: .init(backgroundImage: .init(size: CGSize(width: 24, height: 24), image: .asset(Image(systemName: "star.fill"))),trailingTitles: .init(.text("Title"), .text("Subtitle"))))
+        sut.display(
+            leadingCard: .init(
+                backgroundImage: .init(size: CGSize(width: 24, height: 24),
+                                       image: .asset(Image(systemName: "star.fill"))),trailingTitles:
+                        .init(.text("Title"),
+                              .text("Subtitle"))))
         
         // THEN
         record(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "NAVBAR_WITH_LEADINGCARD_TRAILINGTITLES_LIGHT")
         record(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "NAVBAR_WITH_LEADINGCARD_TRAILINGTITLES_DARK")
+    }
+    
+    // MARK: - func display(secondaryTrailingImage:) tests
+    func test_navigationBar_with_secondaryTrailingImage() {
+        // GIVEN
+        let sut = makeSUT()
+        
+        // WHEN
+        sut.display(style: .init(
+            backgroundColor: .red,
+            horizontalSpacing: 1.0,
+            primeFont: .boldSystemFont(ofSize: 24),
+            primeColor: .blue,
+            secondaryFont: .systemFont(ofSize: 14),
+            secondaryColor: .green)
+        )
+        
+        let image = Image(systemName: "star.fill")
+        sut.display(secondaryTrailingImage: .some(.init(title: "Image", image: image, height: 24)))
+        
+        // THEN
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "NAVBAR_WITH_SECONDARY_TRAILING_IMAGE_LIGHT")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "NAVBAR_WITH_SECONDARY_TRAILING_IMAGE_DARK")
+    }
+    
+    func test_navigationBar_with_secondaryTrailingImage_onPress() {
+        // GIVEN
+        let sut = makeSUT()
+        
+        // WHEN
+        sut.display(style: .init(
+            backgroundColor: .red,
+            horizontalSpacing: 1.0,
+            primeFont: .boldSystemFont(ofSize: 24),
+            primeColor: .blue,
+            secondaryFont: .systemFont(ofSize: 14),
+            secondaryColor: .green)
+        )
+        
+        let image = Image(systemName: "star.fill")
+        sut.display(secondaryTrailingImage: .some(.init(
+            title: "Image",
+            image: image,
+            height: 24,
+            onPress: { [weak sut] in
+                sut?.backgroundColor = .yellow
+            })
+        ))
+        
+        sut.secondaryTrailingImageWrapperView.contentView.onPress?()
+        
+        // THEN
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "NAVBAR_WITH_SECONDARY_TRAILING_IMAGE_ONTAP_LIGHT")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "NAVBAR_WITH_SECONDARY_TRAILING_IMAGE_ONTAP_DARK")
+    }
+    
+    func test_navigationBar_with_tertiaryTrailingImage() {
+        // GIVEN
+        let sut = makeSUT()
+        
+        // WHEN
+        sut.display(style: .init(
+            backgroundColor: .red,
+            horizontalSpacing: 1.0,
+            primeFont: .boldSystemFont(ofSize: 24),
+            primeColor: .blue,
+            secondaryFont: .systemFont(ofSize: 14),
+            secondaryColor: .green)
+        )
+        
+        let image = Image(systemName: "star.fill")
+        sut.display(tertiaryTrailingImage: .some(.init(
+            title: "Image",
+            image: image,
+            height: 24,
+        )))
+        
+        sut.secondaryTrailingImageWrapperView.contentView.onPress?()
+        
+        // THEN
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "NAVBAR_WITH_TERTIARY_TRAILINGIMAGE_LIGHT")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "NAVBAR_WITH_TERTIARY_TRAILINGIMAGE_DARK")
+    }
+    
+    func test_navigationBar_with_tertiaryTrailingImage_onPress() {
+        // GIVEN
+        let sut = makeSUT()
+        
+        // WHEN
+        sut.display(style: .init(
+            backgroundColor: .red,
+            horizontalSpacing: 1.0,
+            primeFont: .boldSystemFont(ofSize: 24),
+            primeColor: .blue,
+            secondaryFont: .systemFont(ofSize: 14),
+            secondaryColor: .green)
+        )
+        
+        let image = Image(systemName: "star.fill")
+        sut.display(tertiaryTrailingImage: .some(.init(
+            title: "Image",
+            image: image,
+            height: 24,
+            onPress: { [weak sut] in
+                sut?.backgroundColor = .yellow
+            }
+        )))
+        
+        sut.tertiaryTrailingImageWrapperView.contentView.onPress?()
+        
+        // THEN
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "NAVBAR_WITH_TERTIARY_TRAILINGIMAGE_ONPRESS_LIGHT")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "NAVBAR_WITH_TERTIARY_TRAILINGIMAGE_ONPRESS_DARK")
+    }
+    
+    func test_navigationBar_with_tertiaryAndSecondary_trailingImages() {
+        // GIVEN
+        let sut = makeSUT()
+        
+        // WHEN
+        sut.display(style: .init(
+            backgroundColor: .red,
+            horizontalSpacing: 1.0,
+            primeFont: .boldSystemFont(ofSize: 24),
+            primeColor: .blue,
+            secondaryFont: .systemFont(ofSize: 14),
+            secondaryColor: .green)
+        )
+        
+        let image = Image(systemName: "star.fill")
+        
+        sut.display(tertiaryTrailingImage: .some(.init(
+            title: "Tert",
+            image: image,
+            height: 24,
+            onPress: { [weak sut] in
+                sut?.backgroundColor = .yellow
+            })
+        ))
+        
+        sut.display(secondaryTrailingImage: .some(.init(
+            title: "Second",
+            image: image,
+            height: 24,
+            onPress: { [weak sut] in
+                sut?.backgroundColor = .yellow
+            })
+        ))
+        
+        sut.secondaryTrailingImageWrapperView.contentView.onPress?()
+        
+        // THEN
+        record(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "NAVBAR_WITH_TERTIARY_SECONDARY_TRAILINGIMAGE_LIGHT")
+        record(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "NAVBAR_WITH_TERTIARY_SECONDARY_TRAILINGIMAGE_DARK")
     }
     
 }
