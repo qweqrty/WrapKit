8b6b4ad4fae72c9617ac6f1a5c1c96d70e13ba6b
Urmatbek Marat Uulu <umaratuulu@mac-MY2QGQ5Y44.local>
2025-11-12 15:33:32 +0600

    Progressbar snapshot tests

---
+++ WrapKitCore/Tests/WrapKitiOStests/UIKitTests/ProgressBarSnapshotTests/ProgressBarSnapshotTests.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBarSnapshotTests.swift
+//  WrapKitTests
+//
+//  Created by Urmatbek Marat Uulu on 12/11/25.
+//
+
+import WrapKit
+import WrapKitTestUtils
+import XCTest
+
+final class ProgressBarSnapshotTests: XCTestCase {
+    
+}
+
+extension ProgressBarSnapshotTests {
+    
+}
