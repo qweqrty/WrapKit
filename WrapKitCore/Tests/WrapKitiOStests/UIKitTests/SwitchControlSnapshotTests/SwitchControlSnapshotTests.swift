fce380a2aeaba55c710888e9e96389e50f233b30
Urmatbek Marat Uulu <umaratuulu@mac-MY2QGQ5Y44.local>
2025-11-14 14:15:05 +0600

    SwitchControl snapshot tests

---
+++ WrapKitCore/Tests/WrapKitiOStests/UIKitTests/SwitchControlSnapshotTests/SwitchControlSnapshotTests.swift
@@ -0,0 +1,46 @@
+//
+//  SwitchControlSnapshotTests.swift
+//  WrapKitTests
+//
+//  Created by Urmatbek Marat Uulu on 14/11/25.
+//
+
+import WrapKit
+import WrapKitTestUtils
+import XCTest
+
+final class SwitchControlSnapshotTests: XCTestCase {
+    func test_switchControl_default_state() {
+        
+    }
+}
+
+extension SwitchControlSnapshotTests {
+    func makeSUT(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (sut: SwitchControl, container: UIView) {
+        
+        let sut = SwitchControl()
+        let container = makeContainer()
+        
+        container.addSubview(sut)
+        sut.anchor(
+            .top(container.topAnchor, constant: 0, priority: .required),
+            .leading(container.leadingAnchor, constant: 0, priority: .required),
+            .trailing(container.trailingAnchor, constant: 0, priority: .required),
+        )
+        
+        container.layoutIfNeeded()
+        
+        checkForMemoryLeaks(sut, file: file, line: line)
+        return (sut, container)
+    }
+    
+    func makeContainer() -> UIView {
+        let container = UIView()
+        container.frame = CGRect(x: 0, y: 0, width: 390, height: 300)
+        container.backgroundColor = .clear
+        return container
+    }
+}
