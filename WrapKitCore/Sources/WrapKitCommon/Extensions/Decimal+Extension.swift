cf567dc1d7a5818e1805e9e5708fb08c9ed6a567
dastan1302@gmail.com <Dastan Mamyrov>
2025-12-09 17:20:29 +0600

    handle conflicts

---
+++ WrapKitCore/Sources/WrapKitCommon/Extensions/Decimal+Extension.swift
@@ -0,0 +1,64 @@
+//
+//  Decimal+Extension.swift
+//  WrapKit
+//
+//  Created by Dastan Mamyrov on 9/12/25.
+//
+
+import Foundation
+
+public extension String {
+    func asDecimal() -> Decimal? {
+        Decimal(string: self)
+    }
+}
+
+public extension Decimal {
+    var nsDecimal: NSDecimalNumber {
+        self as NSDecimalNumber
+    }
+    
+    var doubleValue: Double {
+        nsDecimal.doubleValue
+    }
+    
+    var intValue: Int {
+        nsDecimal.intValue
+    }
+    
+    func asString(
+        withDecimalPlaces count: Int = 0,
+        decimalSeparator: String? = nil,
+        groupingSeparator: String? = nil,
+        locale: Locale = .current
+    ) -> String {
+        return self.asString(minimumFractionDigits: count, maximumFractionDigits: count, decimalSeparator: decimalSeparator, groupingSeparator: groupingSeparator, locale: locale)
+    }
+    
+    func asString(
+        minimumFractionDigits: Int = 0,
+        maximumFractionDigits: Int = 0,
+        decimalSeparator: String? = nil,
+        groupingSeparator: String? = nil,
+        locale: Locale = .current
+    ) -> String {
+        if self.isNaN { return String(Double.nan) }
+        // Format integer part with grouping according to locale
+        let formatter = NumberFormatter()
+        formatter.locale = locale
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = minimumFractionDigits
+        formatter.maximumFractionDigits = maximumFractionDigits
+        formatter.roundingMode = .down // truncate
+        formatter.usesGroupingSeparator = true
+        if let groupingSeparator {
+            formatter.groupingSeparator = groupingSeparator
+        }
+        if let decimalSeparator {
+            formatter.decimalSeparator = decimalSeparator
+        }
+        formatter.groupingSize = 3
+        
+        return formatter.string(from: NSDecimalNumber(decimal: self)) ?? String(format: "%.\(minimumFractionDigits)f", NSDecimalNumber(decimal: self))
+    }
+}
--- WrapKitCore/Sources/WrapKitCommon/Extensions/Double+Extensions.swift
+++ WrapKitCore/Sources/WrapKitCommon/Extensions/Double+Extensions.swift
@@ -7,50 +7,6 @@
 
 import Foundation
 
-public extension String {
-    func asDecimal() -> Decimal? {
-        Decimal(string: self)
-    }
-}
-
-public extension Decimal {
-    func asString(
-        withDecimalPlaces count: Int = 0,
-        decimalSeparator: String? = nil,
-        groupingSeparator: String? = nil,
-        locale: Locale = .current
-    ) -> String {
-        return self.asString(minimumFractionDigits: count, maximumFractionDigits: count, decimalSeparator: decimalSeparator, groupingSeparator: groupingSeparator, locale: locale)
-    }
-    
-    func asString(
-        minimumFractionDigits: Int = 0,
-        maximumFractionDigits: Int = 0,
-        decimalSeparator: String? = nil,
-        groupingSeparator: String? = nil,
-        locale: Locale = .current
-    ) -> String {
-        if self.isNaN { return String(Double.nan) }
-        // Format integer part with grouping according to locale
-        let formatter = NumberFormatter()
-        formatter.locale = locale
-        formatter.numberStyle = .decimal
-        formatter.minimumFractionDigits = minimumFractionDigits
-        formatter.maximumFractionDigits = maximumFractionDigits
-        formatter.roundingMode = .down // truncate
-        formatter.usesGroupingSeparator = true
-        if let groupingSeparator {
-            formatter.groupingSeparator = groupingSeparator
-        }
-        if let decimalSeparator {
-            formatter.decimalSeparator = decimalSeparator
-        }
-        formatter.groupingSize = 3
-        
-        return formatter.string(from: NSDecimalNumber(decimal: self)) ?? String(format: "%.\(minimumFractionDigits)f", NSDecimalNumber(decimal: self))
-    }
-}
-
 public extension Double {
     @available(
         *,
