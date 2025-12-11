5daa0d008645db9ede13a155e69920b6bf0ea394
Dastan <dastan1302@gmail.com>
2025-12-11 19:03:00 +0600

    Decimal Decoder Optional

--- WrapKitCore/Sources/WrapKitCommon/Extensions/Decimal+Decodable.swift
+++ WrapKitCore/Sources/WrapKitCommon/Extensions/Decimal+Decodable.swift
@@ -23,7 +23,6 @@ public struct PreciseDecimal: Codable {
             self.wrappedValue = decimal
         } else if let intValue = try? container.decode(Int.self) {
             self.wrappedValue = Decimal(intValue)
-            return
         } else {
             let doubleValue = try container.decode(Double.self)
             self.wrappedValue = Decimal(doubleValue)
@@ -34,5 +33,40 @@ public struct PreciseDecimal: Codable {
         var container = encoder.singleValueContainer()
         // Encode as string to preserve precision
         try container.encode(wrappedValue.description)
+    }
+}
+
+@propertyWrapper
+public struct PreciseDecimalOptional: Codable {
+    public private(set) var wrappedValue: Decimal?
+    
+    public init(wrappedValue: Decimal?) {
+        self.wrappedValue = wrappedValue
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        
+        if container.decodeNil() {
+            self.wrappedValue = nil
+        } else if let stringValue = try? container.decode(String.self),
+           let decimal = Decimal(string: stringValue) {
+            self.wrappedValue = decimal
+        } else  if let intValue = try? container.decode(Int.self) {
+            self.wrappedValue = Decimal(intValue)
+        } else if let doubleValue = try? container.decode(Double.self) {
+            self.wrappedValue = Decimal(doubleValue)
+        } else {
+            self.wrappedValue = nil
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        if let value = wrappedValue {
+            try container.encode(value.description)
+        } else {
+            try container.encodeNil()
+        }
     }
 }
