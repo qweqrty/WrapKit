ab77e56fdfb10653bb9bbd3b34409856f9f4523b
Ulan Beishenkulov <ubeishenkulov@mac-G3XL27NJTQ.local>
2025-12-26 14:17:55 +0600

    Added test for display(html)

--- WrapKitCore/Tests/WrapKitiOStests/UIKitTests/LabelTests/LabelSnapshotTests.swift
+++ WrapKitCore/Tests/WrapKitiOStests/UIKitTests/LabelTests/LabelSnapshotTests.swift
@@ -1063,6 +1063,24 @@ final class LabelSnapshotTests: XCTestCase {
             assertFail(snapshot: container.snapshot(for: .iPhone(style: .dark)), named: "iOS18.5_\(snapshotName)_DARK")
         } 
     }
+    
+    func test_label_output_html() {
+        // GIVEN
+        let (sut, container) = makeSUT()
+        let snapshotName = "LABEL_HTML"
+        
+        // THEN
+        sut.display(htmlString: "<br>For test text<b>", font: .systemFont(ofSize: 13), color: .red)
+        
+        // THEN
+        if #available(iOS 26, *) {
+            assert(snapshot: container.snapshot(for: .iPhone(style: .light)), named: "iOS26_\(snapshotName)_LIGHT")
+            assert(snapshot: container.snapshot(for: .iPhone(style: .dark)), named: "iOS26_\(snapshotName)_DARK")
+        } else {
+            assert(snapshot: container.snapshot(for: .iPhone(style: .light)), named: "iOS18.5_\(snapshotName)_LIGHT")
+            assert(snapshot: container.snapshot(for: .iPhone(style: .dark)), named: "iOS18.5_\(snapshotName)_DARK")
+        }
+    }
 }
 
 extension LabelSnapshotTests {
