1417c848d33f13e8d904a6873124c1341e37e23f
Dastan Mamyrov <dastan1302@gmail.com>
2026-01-21 14:06:39 +0600

    LKAPP-6653 [iOS] - в деталях дочернего номера отображать блоки сразу, c тенями (#201)
    * DispatchQueue runOnMainThread
    
    * TableView fix adjustHeight missing contentInset

---
+++ WrapKitCore/Sources/WrapKitCommon/Extensions/DispatchQueue+Ext.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public extension DispatchQueue {
+    static func runOnMainThread(_ completion: @escaping () -> Void) {
+        if Thread.isMainThread {
+            completion()
+        } else {
+            DispatchQueue.main.async(execute: completion)
+        }
+    }
+}
