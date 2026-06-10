eb4d5fe7ccdc9ddd2f6df2df8d7686d0c27bb01b
Dastan Mamyrov <dastan1302@gmail.com>
2026-06-10 16:06:55 +0600

    SnapKit 5.7.0

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/onevcat/Kingfisher.git", exact: "7.12.0"),
         .package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "4.0.0"),
         .package(url: "https://github.com/devicekit/DeviceKit", from: "5.7.0"),
-        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "6.0.0"),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.0"),
     ],
     targets: [
         .target(
