87c478df3589a624122ead07210f6d1935a5b5c9
Dastan Mamyrov <dastan1302@gmail.com>
2026-06-17 15:30:16 +0600

    make wrapKit dynamic

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     products: [
         .library(
             name: "WrapKit",
+            type: .dynamic,
             targets: ["WrapKit"]),
         .library(
             name: "WrapKitGame",
