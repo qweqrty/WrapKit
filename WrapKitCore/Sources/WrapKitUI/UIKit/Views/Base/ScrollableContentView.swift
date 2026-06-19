b27cbcb2d8c4b6851f550ef31e70eacf338878f7
Urmabek Marat Uuulu <umaratuulu@nurtelecom.kg>
2026-06-19 12:35:21 +0600

    MYO-7090: ScrollableContentView minHeightConstraint added

--- WrapKitCore/Sources/WrapKitUI/UIKit/Views/Base/ScrollableContentView.swift
+++ WrapKitCore/Sources/WrapKitUI/UIKit/Views/Base/ScrollableContentView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 open class ScrollableContentView: UIScrollView {
     public let contentView = ViewUIKit()
+    private var minHeightConstraint: NSLayoutConstraint?
     
     public init(contentInset: UIEdgeInsets = .zero) {
         super.init(frame: .zero)
@@ -42,10 +43,21 @@ extension ScrollableContentView {
     
     private func setupConstraints() {
         contentView.fillSuperview()
-        contentView.anchor(
-            .widthTo(widthAnchor, 1),
-            .heightTo(safeAreaLayoutGuide.heightAnchor, 1, priority: UILayoutPriority(rawValue: 250))
+        contentView.anchor(.widthTo(widthAnchor, 1))
+        
+        let constraint = contentView.heightAnchor.constraint(
+            equalTo: safeAreaLayoutGuide.heightAnchor
         )
+        constraint.priority = UILayoutPriority(250)
+        constraint.isActive = true
+        minHeightConstraint = constraint
+    }
+    
+    open override func adjustedContentInsetDidChange() {
+        super.adjustedContentInsetDidChange()
+        let extraTop = max(0, adjustedContentInset.top - safeAreaInsets.top)
+        let extraBottom = max(0, adjustedContentInset.bottom - safeAreaInsets.bottom)
+        minHeightConstraint?.constant = -(extraTop + extraBottom)
     }
 }
 #endif
