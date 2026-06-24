f31c9e6158ffd82e89b66fca93fdf272dcc5447a
IsagulovUrmat <75128491+IsagulovUrmat@users.noreply.github.com>
2026-06-24 11:42:40 +0600

    MYO-7053: Added AspectFitImageView.swift (#249)
    Co-authored-by: Urmabek Marat Uuulu <umaratuulu@nurtelecom.kg>

---
+++ WrapKitCore/Sources/WrapKitUI/UIKit/Views/Base/AspectFitImageView.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+final class AspectFitImageView: ImageView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentMode = .scaleAspectFit
+    }
+    
+    convenience init() {
+        self.init(frame: .zero)
+    }
+    
+    @MainActor required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        guard let image = image, image.size.height > 0 else {
+            return super.intrinsicContentSize
+        }
+        let ratio = image.size.width / image.size.height
+        if frame.height > 0 {
+            return CGSize(width: (frame.height * ratio).rounded(), height: frame.height)
+        } else if frame.width > 0 {
+            return CGSize(width: frame.width, height: (frame.width / ratio).rounded())
+        } else {
+            return super.intrinsicContentSize
+        }
+    }
+}
