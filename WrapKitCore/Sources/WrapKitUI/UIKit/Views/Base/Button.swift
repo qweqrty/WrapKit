176a53319b2666882b4e8d6a60e86d85cc38cad8
Urmabek Marat Uuulu <umaratuulu@nurtelecom.kg>
2026-07-08 20:26:17 +0600

    LKAPP-6552: Button backgroundColor fix

--- WrapKitCore/Sources/WrapKitUI/UIKit/Views/Base/Button.swift
+++ WrapKitCore/Sources/WrapKitUI/UIKit/Views/Base/Button.swift
@@ -228,6 +228,7 @@ extension Button: ButtonOutput {
 //            }
             
             self.configuration = config
+            self.backgroundColor = .clear
 
             var isPreviouslyHighlighted = false
             configurationUpdateHandler = { [weak self] button in
